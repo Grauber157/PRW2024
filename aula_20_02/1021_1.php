ae38e1c1690b31303f6242640ad1163cc5e3d48c
Gabriel Faria Dias <faria.dias@aluno.ifsp.edu.br>
2024-02-22 22:02:02 -0300

    Add files via upload

---
+++ aula_20_02/1021_1.php
@@ -0,0 +1,29 @@
+<?php
+    $notas = [100, 50, 20, 10, 5, 2];
+    $moedas = [1, 0.5, 0.25, 0.1, 0.05, 0.01];
+    $N = readline();
+    //ARRAYS NOTAS
+    foreach ($notas as $nota)
+    {
+        $quantidadeNota[] = $N/$nota;
+        $N = fmod($N, $nota);
+    }
+    //PRINT NOTAS
+    echo "NOTAS:\n";
+    foreach ($quantidadeNota as $index => $quantNota)
+    {
+        echo intval($quantNota)." nota(s) de R$ ".number_format($notas[$index],2)."\n";
+    }
+    //ARRAYS MOEDAS
+    foreach ($moedas as $moeda)
+    {
+        $quantidadeMoeda[] = $N/$moeda;
+        $N = fmod($N, $moeda);
+    }
+    //PRINT MOEDAS
+    echo "MOEDAS:\n";
+    foreach ($quantidadeMoeda as $index => $quantMoeda)
+    {
+        echo $quantMoeda." moeda(s) de R$ ".number_format($moedas[$index],2)."\n";
+    }
+?>
