8052096b9a8794d1a70dc742e3b59fa087488b30
Grauber157 <faria.dias@aluno.ifsp.edu.br>
2024-02-27 11:29:19 -0300

    aula_27_02

--- aula_27_02/exe1.php
+++ aula_27_02/exe1.php
@@ -1,15 +1,12 @@
 <?php
     $campoEmail = $_GET['email'];
     $campoSenha = $_GET['senha'];
-    
     $email = "admin";
     $senha = "admin";
-    
     if($campoEmail == $email && $campoSenha == $senha)
     {
         echo "Login Validado!";
     }
-
     else
     {
         echo "Login Inválido!";
