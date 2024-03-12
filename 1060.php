e34c825cb00d7c1aee3822d7c581dc6dc351b268
Gabriel Faria Dias <faria.dias@aluno.ifsp.edu.br>
2024-03-12 12:19:49 -0300

    Terminar Lista 3 12/03/2024

---
+++ 1060.php
@@ -0,0 +1,12 @@
+<?php
+    for ($i = 0; $i < 6; $i++) 
+    {
+        $valor[] = readline();
+    }
+    
+    //terminar o foreach, para comparar cada valor do vetor $valor[]
+    foreach ($numero $valor)
+    {
+        
+    }
+?>
